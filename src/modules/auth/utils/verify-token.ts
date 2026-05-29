fd4ae6183053c7a4489afd104e299c7e8c6a2cb1
erabossco <erabossbd@gmail.com>
2026-05-29 15:11:21 +0600

    feat(auth-utils): add JWT verify-token utility

---
+++ src/modules/auth/utils/verify-token.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { envConfig } from "@/config/env/index.js";
+import type { JwtPayload } from "../types/auth.types.js";
+import { TOKEN_TYPE } from "../constants/auth.constants.js";
+
+type verifyTokenParams = {
+    token: string;
+    type: keyof typeof TOKEN_TYPE;
+}
+/**
+ * Verify JWT token
+ * 
+ * @param token - JWT token from client request 
+ * @returns decoded payload if token is valid
+ */
+
+export const vefiryToken = ({ token, type }: verifyTokenParams): JwtPayload => {
+    let secret: string;
+
+    switch (type) {
+        case "ACCESS":
+            secret = envConfig.jwt.access.secret;
+            break;
+
+        case "REFRESH":
+            secret = envConfig.jwt.refresh.secret;
+            break;
+        default: throw new Error("Invalid token type");
+    }
+    return jwt.verify(token, secret) as JwtPayload;
+}
