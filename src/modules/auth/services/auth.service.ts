3d684260e530965e7e1343908164ff2d49e6a3bd
erabossco <erabossbd@gmail.com>
2026-06-13 12:02:51 +0600

    feat(auth): add user login service

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -2,13 +2,30 @@ import { prisma } from "@/infrastructure/database/prisma/index.js";
 import { passwordService } from "./password.service.js";
 import { jwtService } from "./jwt.service.js";
 import { sessionService } from "./session.service.js";
-import type { AuthResponse, JwtPayload, RegisterContext, RegisterUserPayload } from "../types/auth.types.js";
+import type {
+    AuthResponse,
+    JwtPayload,
+    LoginContext,
+    LoginUserPayload,
+    RegisterContext,
+    RegisterUserPayload
+} from "../types/auth.types.js";
 
 
 class AuthService {
+
+    // ==========================
+    // USER REGISTRATION SERVICE
+    // ==========================
+
     /**
      * Register a new user
+     * 
+     * @param payload (user form data)
+     * @param context (user agent, ip address)
+     * @returns user and tokens
      */
+
     async registerUser(payload: RegisterUserPayload, context: RegisterContext): Promise<AuthResponse> {
 
         // Check if user is already exists 
@@ -64,9 +81,85 @@ class AuthService {
                 refreshToken,
             }
         };
+    };
+
+    // ===========================
+    // LOGIN SERVICE
+    // ===========================
+
+    /**
+     * 
+     * Login user
+     * 
+     * @param payload (user form data) 
+     * @param context (user agent and ip address)
+     * @returns user and tokens 
+     */
+
+    async loginUser(payload: LoginUserPayload, context: LoginContext): Promise<AuthResponse> {
+
+        // Check if user exists.
+        // if no email matches, throw error
+        const user = await prisma.user.findUnique({
+            where: {
+                email: payload.email,
+            }
+        });
+
+        if (!user) {
+            throw new Error("Invalid credentials");
+        }
+
+        // Check if login with password is allowed
+        if (!user.passwordHash) {
+            throw new Error("Password login is unavailable")
+        }
+
+        // If user email exists and login with password is active
+        // check if provided password is correct
+        const isPasswordValid = await passwordService.comparePassword(payload.password, user.passwordHash);
+        if (!isPasswordValid) {
+            throw new Error("Invalid credentials");
+        }
+
+        // Check if the account has been soft-deleted.
+        // The account remains in the database for audit purposes,
+        // but should be treated as non-existent by the application.
+        if (user.isDeleted) {
+            throw new Error("Account not found");
+        }
+
+        // If all data is valid, check if user is ative 
+        if (!user.isActive) {
+            throw new Error("Account disabled");
+        }
+
+        // Create a session with the login data
+        const session = await sessionService.createSession({
+            userId: user.id,
+            ...(context.userAgent && { userAgent: context.userAgent }),
+            ...(context.userAgent && { ipAddress: context.ipAddress }),
+        });
+
+        // Generate access and refresh token
+        const jwtPayload: JwtPayload = {
+            userId: user.id,
+            email: user.email,
+            role: user.role,
+            sessionId: session.id,
+        }
+        const accessToken = jwtService.generateAccessToken(jwtPayload)
+        const refreshToken = jwtService.generateRefreshToken(jwtPayload)
+
+        // Return user and tokens
+        return {
+            user,
+            tokens: {
+                accessToken,
+                refreshToken,
+            }
+        };
     }
 }
 
 export const authService = new AuthService();
-
-
