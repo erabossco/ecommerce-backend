58753888001e90cd699e1199c62b283ab2cb60d4
erabossco <erabossbd@gmail.com>
2026-08-05 21:23:27 +0600

    refactor(routes): refactor use of middlewares in route file

--- src/modules/catalog/brand/routes/brand.routes.ts
+++ src/modules/catalog/brand/routes/brand.routes.ts
@@ -1,25 +1,24 @@
 import { Router } from "express";
 import { brandController } from "../controllers/brand.controller.js";
-import { validateBrandBody, validateBrandId, validateBrandQuery, } from "../middlewares/brand.middleware.js";
 import { brandIdSchema, brandQuerySchema, createBrandSchema, updateBrandSchema } from "../validators/brand.validator.js";
-import { validateBody, validateParams } from "@/shared/middlewares/validate-request.middleware.js";
+import { validateBody, validateParams, validateQuery } from "@/shared/middlewares/validate-request.middleware.js";
 
 
 const router = Router();
 
 
 router.post("/",
-    validateBrandBody(createBrandSchema),
+    validateBody(createBrandSchema),
     brandController.create
 );
 
 router.get("/:id",
-    validateBrandId(brandIdSchema),
+    validateParams(brandIdSchema),
     brandController.findById
 );
 
 router.get("/",
-    validateBrandQuery(brandQuerySchema),
+    validateQuery(brandQuerySchema),
     brandController.findMany
 );
 
